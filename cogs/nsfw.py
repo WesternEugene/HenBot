dc186bfa8461f63d19f88b7768b0b6c8d7c3c867
sumo-boi <kozhushko2006@gmail.com>
2021-05-16 13:58:18 +0500

    Update nsfw.py

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -20,5 +20,26 @@ class Nsfw(commands.Cog):
         else:
             await ctx.send('Не тут.')
 
+    @commands.command()
+    async def gif(self, ctx):
+        if ctx.channel.is_nsfw():
+            await ctx.send(hmtai.useHM('v2', 'gif'))
+        else:
+            await ctx.send('Не тут.')
+
+    @commands.command()
+    async def uniform(self, ctx):
+        if ctx.channel.is_nsfw():
+            await ctx.send(hmtai.useHM('v2', 'uniform'))
+        else:
+            await ctx.send('Не тут.')
+
+    @commands.command()
+    async def cum(self, ctx):
+        if ctx.channel.is_nsfw():
+            await ctx.send(hmtai.useHM('v2', 'cum'))
+        else:
+            await ctx.send('Не тут.')
+
 def setup(bot):
     bot.add_cog(Nsfw(bot))
