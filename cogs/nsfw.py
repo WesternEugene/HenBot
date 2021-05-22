c49b5086e4c8331b4f20954f5b184ae2020ca705
sumo-boi <kozhushko2006@gmail.com>
2021-05-22 11:58:40 +0500

    Update nsfw.py

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -7,68 +7,11 @@ class Nsfw(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commads.command()
+    @commands.command()
     async def rand(self, ctx):
         cat = ['hentai', 'bdsm', 'uniform', 'cum', 'femdom', 'tentacles', 'nsfwNeko', 'ero']
         if ctx.channel.is_nsfw():
             await ctx.send(hmtai.useHM('v2', random.choice(cat)))
-
-    @commands.command()
-    async def hentai(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'hentai'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def bdsm(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'bdsm'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def uniform(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'uniform'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def cum(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'cum'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def femdom(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'femdom'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def tentacles(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'tentacles'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def neko(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'nsfwNeko'))
-        else:
-            await ctx.send('Не тут.')
-
-    @commands.command()
-    async def ero(self, ctx):
-        if ctx.channel.is_nsfw():
-            await ctx.send(hmtai.useHM('v2', 'ero'))
-        else:
-            await ctx.send('Не тут.')
-
 
 def setup(bot):
     bot.add_cog(Nsfw(bot))
